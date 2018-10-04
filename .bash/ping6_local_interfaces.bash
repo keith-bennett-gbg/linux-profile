c8028220904122623e795cfa3e2c441f5d282264
Keith Bennett <kanezor@gmail.com>
2018-10-03 23:53:03 -0500

    add timeout and documentation

--- .bash/ping6_local_interfaces.bash
+++ .bash/ping6_local_interfaces.bash
@@ -6,8 +6,13 @@ ping6_local_interfaces()
 	for iface
 	do
 		search="([0-9a-f]{1,4}::?){1,7}([0-9a-f]{1,4})%.*: "
-		# echo "ping6 -I \"${iface}\" ff02::1 -c 2 | grep "bytes from" | grep -oP \"${search}\" | sed -r 's/: $//g' | sort -u"
-		ping6 -I "${iface}" ff02::1 -c 2 | grep "bytes from" | grep -oP "${search}" | sed -r 's/: $//g' | sort -u
+		# echo "ping6 -I \"${iface}\" ff02::1 -c 2 -w 1 | grep "bytes from" | grep -oP \"${search}\" | sed -r 's/: $//g' | sort -u"
+		#
+		# -I interface
+		# ff02::1 RFC4291 All Nodes Address, Link-Local: https://tools.ietf.org/html/rfc4291
+		# -c count
+		# -w number of seconds to wait for each ping
+		ping6 -I "${iface}" ff02::1 -c 2 -w 1 | grep "bytes from" | grep -oP "${search}" | sed -r 's/: $//g' | sort -u
 	done
 	wait
 }
