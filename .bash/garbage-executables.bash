f5a841399ef96e736d56e72d0fa51e090ddbc576
Keith Bennett <keithb@genebygene.com>
2019-03-25 12:25:53 -0500

    easier to read garbage

--- .bash/garbage-executables.bash
+++ .bash/garbage-executables.bash
@@ -1,9 +1,58 @@
 #!/usr/bin/bash
 
-garbage=( /usr/lib/gvfs/gvfs{-{afc,goa,gphoto2,mtp}-volume-monitor,d-{afc,afp,afp-browse,dav,recent,trash,network,dnssd}} /usr/bin/seahorse /usr/bin/gnome-keyring{,-3,-daemon} /usr/lib/bluetooth/bluetoothd /usr/lib/blueman/blueman-mechanism )
-garbage=( "$( for g in "${garbage[@]}"; do if [ -x "${g}" ]; then echo -n "${g} "; fi; done )" )
+# List of garbage apps
+garbage=(
+	# Gnome Virtual Filesystem
+	# I absolutely DO NOT want Gnome to run daemons for garbage.
+	#
+	# That includes shit like:
+	# afc: iPhone / iPod Touch
+	# afp: Apple Filing Protocol
+	# dav: insecure HTTP WebDAV
+	# dnssd: DNS Service Discovery
+	# ftp: insecure FTP
+	# http: insecure HTTP
+	# gphoto2: GNU Photo
+	# mtd: Media Transfer Protocol
+	# nfs: insecure NFS
+	# smb: Windows Samba
+	# goa: Gnome Online Accounts
+	#
+	# Extra shitty shit like such:
+	# recent: recent://
+	# trash: trash://
+	# network: network://
+	# 
+	/usr/lib/gvfs/gvfs-{afc,afp,afp-browse,dav,goa,gphoto2,mtp,smb,smb-browse,recent,trash,network,dnssd}-volume-monitor
+	/usr/lib/gvfs/gvfsd-{afc,afp,afp-browse,dav,goa,gphoto2,mtp,smb,smb-browse,recent,trash,network,dnssd}
+
+	# Any sort of credential-caching "agent" whatsoever is an anti-pattern.
+	# -> ssh-agent
+	# -> seahorse
+	# -> gnome-keyring
+	/usr/bin/ssh-agent
+	/usr/bin/seahorse
+	/usr/bin/gnome-keyring{,-3,-daemon}
+
+	# Autostarting bluetooth garbage
+	/usr/lib/bluetooth/bluetoothd
+	/usr/lib/blueman/blueman-mechanism
+)
+
+# Filter list on whether garbage is executable
+garbage=( "$(
+	for g in "${garbage[@]}"
+	do
+		if [ -x "${g}" ]
+		then
+			echo -n "${g} "
+		fi
+	done
+)" )
+
+# If any are, then tell me.
 if [ "${#garbage}" -gt 0 ]
 then
-	echo "~/.bashrc: garbage is executable: ${garbage[@]}"
+	>&2 echo "~/.bashrc: garbage is executable: ${garbage[@]}"
 	exit 1
 fi
